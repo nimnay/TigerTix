ccf85a583df32020713fa6049a993e0c80c00a81
Nimra Nayyar <nimranayyar05@gmail.com>
2025-09-09 15:05:36 -0400

    port change

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,6 @@ const app = express();
 const routes = require('./routes/routes');
 app.use(cors());
 app.use('/api', routes);
-const PORT = 5000;
+const PORT = 8080;
 app.listen(PORT, () => console.log(`Server running at
 http://localhost:${PORT}`));
