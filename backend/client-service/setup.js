2b2e2e0e5b0ce577001f10a491ec2a4ce54362d6
Angie Diaz <adiazhe@clemson.edu>
2025-10-06 22:31:44 -0400

    added ignoring, tweaking server setup

--- backend/client-service/setup.js
+++ backend/client-service/setup.js
@@ -6,7 +6,16 @@ const path = require('path');
 const sqlite3 = require('sqlite3').verbose();   
 const dbPath = path.resolve(__dirname, '../../shared-db/database.sqlite');
 
-dbPath.serialize((() => {
+//Connect to shared database
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error('Error opening database:', err.message);
+  } else {
+    console.log('Connected to shared SQLite database');
+  }
+});
+
+db.serialize((() => {
     db.run(`
         CREATE TABLE IF NOT EXISTS events (
             id INTEGER PRIMARY KEY,
@@ -20,11 +29,11 @@ dbPath.serialize((() => {
             console.error('Error creating events table', err);
         } else {
             console.log('Events table ensured in database');
+            db.close();
         }
     });
 }) );
 
 console.log('Database setup complete');
-//Export the database connection for reuse if needed
-module.exports = db;
+
 
