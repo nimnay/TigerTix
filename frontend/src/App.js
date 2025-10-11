7e41bfda95db08d68f148c9bcdc11ff417f66fd8
Diana Sanchez <dianasanchezcruz17@gmail.com>
2025-10-10 23:11:24 -0400

    changed ports, now actually connected to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,13 @@ function App() {
   const [events, setEvents] = useState([]);
   const [message, setMessage] = useState("");
   useEffect(() => {
-    fetch('http://localhost:8080/api/events')
+    fetch('http://localhost:6001/api/events')
     .then((res) => res.json())
     .then((data) => setEvents(data))
     .catch((err) => console.error(err));
 }, []);
 const buyTicket = (eventId) => {
-  fetch(`http://localhost:8080/api/purchase/${eventId}`, {
+  fetch(`http://localhost:6001/api/purchase/${eventId}`, {
     method: "POST",
   })
     .then((res) => {
@@ -22,7 +22,7 @@ const buyTicket = (eventId) => {
       setMessage("Ticket purchased successfully!");
 
       // Option 1: Re-fetch all events (simple)
-      return fetch("http://localhost:8080/api/events")
+      return fetch("http://localhost:6001/api/events")
         .then((res) => res.json())
         .then((updated) => setEvents(updated));
     })
