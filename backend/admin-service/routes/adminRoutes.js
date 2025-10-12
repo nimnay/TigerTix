68d1eaa4e99b8ecb0b70ece19906ffba60230ba1
Nimra Nayyar <nimranayyar05@gmail.com>
2025-10-11 23:59:41 -0400

    route update

--- backend/admin-service/routes/adminRoutes.js
+++ backend/admin-service/routes/adminRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 
 
-router.post('/events', adminController.createEvent);
+router.post('/api/events', adminController.createEvent);
 
 module.exports = router;
