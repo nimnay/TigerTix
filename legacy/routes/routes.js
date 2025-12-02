895c353e85634ab857470fdf844763e6224b66d4
Angie Diaz <adiazhe@clemson.edu>
2025-12-02 14:41:56 -0500

    cleanup: legacy removed

--- legacy/routes/routes.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { listEvents } = require('../../backend/controllers/controller');
-router.get('/events', listEvents);
-module.exports = router;
